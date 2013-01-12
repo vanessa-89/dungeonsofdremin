555fb1be587332f4002666722b6fefb915d6ac5c
sh4773r <SH4773R@gmail.com>
2013-01-12 00:17:57 +0000

    Added a health restoring item type into the items code, added some code for making circles(not working) and lines. Remade tilemanager, added per pixel support for lighting

--- tilemanager.hpp
+++ tilemanager.hpp
@@ -10,107 +10,163 @@ using namespace std;
 namespace tiles
 {
 
-	class tile
-	{
-		public:
-		SDL_Color dat[16][16]; // Store color data
-		vector<int> contents; // Item ID's stored in this tile
+    struct pixeldat
+    {
+        int r;
+		int g;
+		int b;
+		bool alpha;
 
-        // Lighting flags and values
-		bool lightenabled; // Can this tile be lit?
-
-		bool collision; // Collide with this tile?
-
-		tile()
-		{
-
-		}
+        pixeldat()
+        {
+			alpha = false;
+			//r = 0;
+			//g = 0;
+			//b = 0;
+        }
 		
-		tile& operator=(const tile& ti)
-		{
-			
-			for(int sx = 0; sx < 16; sx++)
-			{
-				for(int sy = 0; sy < 16; sy++)
-				{
-
-					dat[sx][sy] = ti.dat[sx][sy];
-				}
-			}
-			
-			return *this;
-		}
-		
-		
-		void clear()
-		{
-			//for(int sx = 0; sx < 16; sx++)
-			{
-				//for(int sy = 0; sy < 16; sy++)
-				{
-					// Clear each xy combonation
-					//dat[sx][sy].r = 0;
-					//dat[sx][sy].g = 0;
-					//dat[sx][sy].b = 0;
-					
-				}
-			}
-	
-		}
-
-		void draw(SDL_Surface* sur, int xpos, int ypos)
-		{
-
-			for(int sx = 0; sx < 16; sx++)
-			{
-				for(int sy = 0; sy < 16; sy++)
-				{
-                    // Output the data with offsets (x and y)
-					//cout << "da" << endl;
-					//cout << dat[sx][sy].r << endl;
-					pix::put_pixel16(sur, sx+xpos, sy+ypos, dat[sx][sy].r, dat[sx][sy].g, dat[sx][sy].b);
-				}
-			}
-		}
-
-	};
-
-	class tilemap
-	{
-		public:
-		tile tiledat[28][21];
-
-		tilemap()
-		{
-
-		}
+		pixeldat(int ar, int ag, int ab, bool aalpha)
+        {
+			alpha = aalpha;
+			r = ar;
+			g = ag;
+			b = ab;
+        }
 		
 		void draw(SDL_Surface* sur, int x, int y)
 		{
-			// For every tile in tiledat, call its draw function with the proper offsets
-			for(int sx = 0; sx < 28; sx++)
-			{
-				for(int sy = 0; sy < 21; sy++)
-				{
-					tiledat[x][y].draw(sur, (sx*16)+x, (sy*16)+y);
-				}
-			}
+			pix::put_pixel16(sur, x, y, r, g, b );
 		}
+
+    };
+	
+	
+	struct tiledat
+	{
+		pixeldat data[16][16];
+		int x;
+		int y;
+		
 		
 		void clear()
 		{
-			//for(int x = 0; x < 28; x++)
+			for(int xx = 0; xx < 16; xx++)
 			{
-				///for(int y = 0; y < 21; x++)
+				for(int yy = 0; yy < 16; yy++)
 				{
-					//tiledat[x][y].clear();
+					data[xx][yy].alpha = false;
+					data[xx][yy].r = 0;
+					data[xx][yy].g = 0;
+					data[xx][yy].b = 0;
 				}
 			
 			}
 		}
-
+		
+		tiledat()
+		{
+			//clear();
+		}
+		
+		void draw(SDL_Surface* sur)
+		{
+			for(int xx = 0; xx < 16; xx++)
+			{
+				for(int yy = 0; yy < 16; yy++)
+				{
+					data[xx][yy].draw(sur, xx+x,yy+y); // Draw with offsets
+				}
+			}
+		}
+		
+	
 	};
-
+		
+		
+	struct tilemap
+	{
+		vector<tiledat> tiledata;
+		int xsize;
+		int ysize;
+		int xoff;
+		int yoff;
+		
+		tilemap(int axsize, int aysize, int axoff, int ayoff)
+		{
+			ysize = axsize;
+			xsize = aysize;
+			xoff = axoff;
+			yoff = ayoff;
+			tiledata.resize(xsize*ysize);
+			clear();
+		}
+		
+		void clear()
+		{
+			for(int xx = 0; xx < xsize; xx++)
+			{
+				for(int yy = 0; yy < ysize; yy++)
+				{
+					tiledata[xsize * xx + yy].clear();
+					tiledata[xsize * xx + yy].x = xx*16+xoff;
+					tiledata[xsize * xx + yy].y = yy*16+yoff;
+				}
+			}
+		}
+		
+		void writeTile(tiledat* tile, int xpos, int ypos)
+		{
+			for(int tx = 0; tx < 16; tx++)
+			{
+				for(int ty = 0; ty < 16; ty++)
+				{
+					tiledata[xsize * xpos + ypos].data[tx][ty].r = tile->data[tx][ty].r;
+					tiledata[xsize * xpos + ypos].data[tx][ty].g = tile->data[tx][ty].g;
+					tiledata[xsize * xpos + ypos].data[tx][ty].b = tile->data[tx][ty].b;
+				}
+			}
+		}
+		
+		void draw(SDL_Surface* sur)
+		{
+			for(int xx = 0; xx < xsize; xx++)
+			{
+				for(int yy = 0; yy < ysize; yy++)
+				{
+					tiledata[xsize * xx + yy].draw(sur);
+				}
+			}
+		
+		}
+		
+	
+	};
+	
+	struct pointLight
+	{
+		int x;
+		int y;
+		int radius;
+		int brightness;
+		
+		pointLight()
+		{
+			radius = 0;
+			brightness = 0;
+			x = 0;
+			y = 0;
+		}
+		
+		void calculateCircle()
+		{
+			
+		
+		}
+	
+	};
+	
+	
+	
 }
 
 #endif
